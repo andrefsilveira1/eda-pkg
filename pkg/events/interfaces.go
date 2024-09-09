f0a744d4b308b8ecb28dfb4215c7da6660463269
André Freitas <freitasandre38@gmail.com>
2024-09-09 06:21:17 -0300

    feat: create event handler and event dispatcher interface

--- pkg/events/interfaces.go
+++ pkg/events/interfaces.go
@@ -7,3 +7,15 @@ type EventInterface interface {
 	GetDate() time.Time
 	GetPayload() interface{}
 }
+
+type EventHandlerInterface interface {
+	Handle(event EventInterface)
+}
+
+type EventDispatcherInterface interface {
+	Register(eventName string, handler EventHandlerInterface)
+	Dispatch(event EventInterface)
+	Remove(eventName string, handler EventHandlerInterface)
+	Has(eventName string, handler EventHandlerInterface) bool
+	Clear()
+}
